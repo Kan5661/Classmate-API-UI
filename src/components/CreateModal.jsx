30bbad5b83dd9f83437a449989ab3d13b3861433
Kan5661 <linkan0718@gmail.com>
2023-01-19 20:26:01 -0500

    create modal

---
+++ src/components/CreateModal.jsx
@@ -0,0 +1,9 @@
+function CreateGame(prop) {
+    return(
+        <div className={prop.createActive? 'createScreen' : 'hideElement'}>
+
+        </div>
+    )
+}
+
+export default CreateGame
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,16 @@
 import Menu from "./Menu.jsx"
+import CreateGame from './CreateModal.jsx'
 import { useState } from 'react'
 
 function Nav() {
 
     const [menuActive, setMenuActive] = useState(false)
+    const [createActive, setCreateActive] = useState(false)
     function handleMenu() {
         menuActive? setMenuActive(false) : setMenuActive(true)
+    }
+    function handleCreate() {
+        createActive? setCreateActive(false) : setCreateActive(true)
     }
 
     return(
@@ -15,9 +20,12 @@ function Nav() {
                 <line x1="12" y1="25" x2="38" y2="25" stroke="rgb(92,233,255)" strokeWidth="4" />
                 <line x1="10" y1="35" x2="40" y2="35" stroke="rgb(92,233,255)" strokeWidth="3" />
             </svg>
+            <div onClick={() => handleCreate()} className='createBtn'>Create</div>
             <h1>Free to Play Games</h1>
             <Menu menuActive={menuActive}/>
+            <CreateGame createActive={createActive} />
         </div>
+        
     )
 }
 
